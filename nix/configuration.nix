1f8aad8504ccd3883a86b09d35b6903d4533538a
Cy Pokhrel <cy@cy7.sh>
2024-12-06 14:37:28 -0500

    add nextcloud

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -239,6 +239,20 @@ in {
     };
   };
 
+  systemd.services.nextcloud-mount = {
+    enable = true;
+    description = "Mount the nextcloud data remote";
+    after = [ "network-online.target" ];
+    requires = [ "network-online.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      Type = "notify";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount --config /home/yt/.config/rclone/rclone.conf --uid 33 --gid 0 --allow-other --file-perms 0770 --dir-perms 0770 --transfers=32 rsyncnet:nextcloud /mnt/nextcloud";
+      ExecStop = "/bin/fusermount -u /mnt/nextcloud";
+      EnvironmentFile = "/run/secrets/rclone";
+    };
+  };
+
   services.invidious = {
     enable = true;
     address = "127.0.0.1";
