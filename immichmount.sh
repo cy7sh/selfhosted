287a6a1a10f2532777cbd71fb7f3f3bc929b404e
Cy Pokhrel <cy@cy7.sh>
2024-12-06 22:47:49 -0500

    delete unused scripts

--- immichmount.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# export RCLONE_CONFIG_PASS=$(pass show rclone/crashtan)
-config=/home/yt/.config/rclone/rclone.conf
-
-sudo -E rclone mount --config $config --daemon --dir-cache-time 720h --poll-interval 0 --vfs-cache-mode writes photos: /mnt/photos
