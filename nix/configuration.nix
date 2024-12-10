17cde43884f0d54573b19a638f20149bbe5017e8
cy <hi@cything.io>
2024-12-10 13:56:29 -0500

    bring back redlib

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -280,5 +280,15 @@ in {
       protocolUseSSL = true;
     };
   };
+
+  services.redlib = {
+    enable = true;
+    port = 8087;
+    address = "127.0.0.1";
+    settings = {
+      # settings are just env vars
+      REDLIB_ENABLE_RSS = true;
+    };
+  };
 }
 
